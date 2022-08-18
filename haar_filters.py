27c71d497b86a0779074cc72a2f76dd89b8a3997
vimal sheoran <vimalpsheoran@gmail.com>
2022-08-18 07:18:54 +0530

    Added new 3 rectangle and 4 rectangle haar filters.

--- haar_filters.py
+++ haar_filters.py
@@ -27,6 +27,37 @@ def rectangle_horizontal_filter(x, y, black_white):
 	_filter[mask] = 255
 	return np.reshape(_filter, (x, y))
 
+def _3rectangle_feature(x, y, black_white):
+	begin = int((x*y)/3)
+	end = 2*begin
+	if black_white == 'black_white':
+		_filter = np.zeros((x*y), dtype=float)
+		_filter[begin:end] = 255
+	else:
+		_filter = np.full((x*y), 255, dtype=float)
+		_filter[begin:end] = 0
+	return np.reshape(_filter, (x,y))
+
+def _4rectangle_feature(x, y, black_white):
+	_filter = np.zeros((x*y), dtype=float)
+	_filter = np.reshape(_filter, (x, y))
+	if black_white == 'black_white':
+		for i in range(0, int(y/2)):
+			begin = int(x/2); end = x 
+			_filter[i][begin:end] = 255
+		for i in range(int(y/2), y):
+			begin = 0; end = int(x/2);
+			_filter[i][begin:end] = 255
+	else:
+		for i in range(0, int(y/2)):
+			begin = 0; end = int(x/2);
+			_filter[i][begin:end] = 255
+		for i in range(int(y/2), y):
+			begin = int(x/2); end = x 
+			_filter[i][begin:end] = 255
+	return _filter
+
+
 def get_filter(
 	xshape=24, 
 	yshape=24, 
@@ -38,3 +69,9 @@ def get_filter(
 
 	if filter_type == 'rect_hrzn':
 		return rectangle_horizontal_filter(xshape, yshape, black_white)
+
+	if filter_type == '3rect':
+		return _3rectangle_feature(xshape, yshape, black_white)
+
+	if filter_type == '4rect':
+		return _4rectangle_feature(xshape, yshape, black_white)
